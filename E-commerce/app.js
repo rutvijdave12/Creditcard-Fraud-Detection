221ea9c4c4d4f62bbed4ef506f92a80050d7e46a
Shaunak Alshi <you@example.com>
2021-04-12 19:19:35 +0530

    Second Commit

--- E-commerce/app.js
+++ E-commerce/app.js
@@ -3,23 +3,44 @@ const bodyParser=require('body-parser');
 // const multer=require('multer');
 const ejs=require('ejs');
 const mongoose=require('mongoose');
-
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose=require('passport-local-mongoose');
 
 const app=express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static('public'));
 
+app.use(session({
+    secret: 'Alohomora',
+    resave: false,
+    saveUninitialized: false
+    // cookie: { secure: true }
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology: true})
-
-const userSchema = {
+mongoose.set('useCreateIndex', true);
+const userSchema = new mongoose.Schema({
     username:String,
     email:String,
     password:String
 
-}
+});
+
+userSchema.plugin(passportLocalMongoose);
+
 const User=new mongoose.model("User", userSchema);//User Data
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 const bookSchema = {
     bookname:String,
@@ -45,13 +66,6 @@ const billSchema = {
 }
 const Bookbuy=new mongoose.model("Bookbuy",billSchema); //billing data
 
-
-
-
-
-
-
-
 app.get('/', (req, res)=>{
     const book=Book.find({}, (err,allBooks)=>{
         if(err){
@@ -67,63 +81,108 @@ app.get('/', (req, res)=>{
 }) 
 
 
-
 app.get('/sign-in', (req, res)=>{
     res.render('sign-in');
 }) 
 
 app.post('/sign-in',(req,res)=>{
-    const password=req.body.password;
-    const email=req.body.email;
+    // const password=req.body.password;
+    // const email=req.body.email;
 
-    User.findOne({email:email},function(err, foundUser){
-        if(err)
-        {
-            console.log("3");
-            console.log(err);
-            console.log("4");
-        }
-        else{
-            if(foundUser){
-                if(foundUser.password === password){
-                   res.redirect('/'+foundUser._id);
-                }else{
-                    console.log("Wrong password");
-                }
+    // User.findOne({email:email},function(err, foundUser){
+    //     if(err)
+    //     {
+    //         console.log("3");
+    //         console.log(err);
+    //         console.log("4");
+    //     }
+    //     else{
+    //         if(foundUser){
+    //             if(foundUser.password === password){
+    //                res.redirect('/'+foundUser._id);
+    //             }else{
+    //                 console.log("Wrong password");
+    //             }
 
-            }else{
-                res.redirect('/sign-up')
-            }
-        }
-    })
+    //         }else{
+    //             res.redirect('/sign-up')
+    //         }
+    //     }
+    // })
 
    
 })
+app.get('/sign-up', (req, res)=>{
+    res.render('sign-up');
+}) 
 
+app.post('/sign-up',(req,res)=>{
+    // const newUser=new User({
+    //     username:req.body.name,
+    //     password:req.body.password,
+    //     email:req.body.email
+    
+    //    });
+
+
+    //    newUser.save(function(err){
+    //        if(err){
+    //         console.log("11");
+    //         console.log(err);
+    //         console.log("12");
+    //        }else{
+    //         res.redirect("/sign-in");
+    //        }    
+    //    })
+
+    User.register({username:req.body.username,email:req.body.mail},req.body.password,function(err,user){
+        
+        if(err){
+            console.log(err)
+            res.redirect("/sign-up");
+        }else{
+            
+            passport.authenticate("local",{session: true})(req,res,function(){
+                res.redirect("/"+user._id);
+
+            })
+        }
+
+    })
+    
+    
+})
+
+var foundBook;
+var users;
 app.get("/:id",(req,res)=>{
-    const book=Book.find({}, (err,allBooks)=>{
+    
+
+        Book.find({}, (err,allBooks)=>{
         if(err){
             console.log("5");
             console.log(err);
             console.log("6");
         }else{
-         foundBook=allBooks;
-        // res.render('index',{books:allBooks});
+        foundBook=allBooks;
         }
     });
-    User.findById(req.params.id,(err,foundUser)=>{
-        if(err){
-            console.log("7");
-            console.log(err)
-            console.log("8");
-        }else{
-            res.render("index_signed",{user:foundUser,books:foundBook});
-        }
-    })
+    
 
+    User.findById(req.params.id,(err,foundUser)=>{
+        
+        users=foundUser;
+        console.log(users);
+    })
+    
+
+    if(req.isAuthenticated()){
+        res.render("index_signed",{books:foundBook,user:users});
+    }else{
+        res.redirect("/sign-in");
+    }
 
 })
-
 
 app.get('/book/:id', (req, res)=>{
     
@@ -139,30 +198,9 @@ app.get('/book/:id', (req, res)=>{
     
 }) 
 
-app.get('/sign-up', (req, res)=>{
-    res.render('sign-up');
-}) 
-
-app.post('/sign-up',(req,res)=>{
-    const newUser=new User({
-        username:req.body.name,
-        password:req.body.password,
-        email:req.body.email
-    
-       });
 
 
-       newUser.save(function(err){
-           if(err){
-            console.log("11");
-            console.log(err);
-            console.log("12");
-           }else{
-            res.redirect("/sign-in");
-           }    
-       })
-    
-})
+
 
 app.get('/:id/bill', (req, res)=>{
     Book.findById(req.params.id,(err,foundBook)=>{
